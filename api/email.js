594878483387ff5a2b56543754d9665269c57628
casian2910 <casianrazvan2910@gmail.com>
2025-09-17 16:47:23 +0200

    Update email.js

--- api/email.js
+++ api/email.js
@@ -16,12 +16,44 @@ export async function sendMail(to, fenstertyp, fluegel, unterlicht, amount) {
     to: to,
     subject: "Ihre Bestellbestätigung",
     html: `
-      <h2>Danke für Ihre Bestellung!</h2>
-      <p><b>Fenstertyp:</b> ${fenstertyp}</p>
-      <p><b>Flügel:</b> ${fluegel}</p>
-      <p><b>Unterlicht:</b> ${unterlicht}</p>
-      <p><b>Gesamtpreis:</b> ${amount} EUR</p>
-      <p>Wir werden uns in Kürze mit Ihnen in Verbindung setzen.</p>
+     <div style="font-family: Arial, sans-serif; line-height: 1.5; color: #333;">
+      <!-- Logo -->
+      <div style="text-align:center; margin-bottom:20px;">
+        <img src="https://cdn.shopify.com/s/files/1/0946/0301/8572/files/logo_transparent.png?v=1747438787" alt="Fenster RTH Logo" style="max-width:200px;">
+      </div>
+
+      <p>Sehr geehrter Kunde,</p>
+      <p>vielen Dank für Ihr Vertrauen, das Sie uns mit Ihrer Bestellung entgegengebracht haben. Wir bestätigen hiermit nachfolgenden Auftrag. Es gelten unsere AGB.</p>
+
+      <p><strong>Bestellnummer:</strong> ${order_number}</p>
+
+      <h3>Rechnungsadresse</h3>
+      <p>
+        ${data.name}<br>
+        ${data.strasse}<br>
+        ${data.plz-ort} ${data.ort}<br>
+        </p>
+
+
+      <h3>Ihre bestellten Artikel</h3>
+      <ul>
+      <li>Hersteller: ${hersteller}</li>
+      <li>Fensterart: ${fensterart}</li>
+      <li>Fensteröffnung: ${fenstertyp}</li>
+      <li>Außenfarbe: ${aussenfarbe}</li>
+      <li>Innenfarbe: ${innenfarbe}</li>
+      <li>Verglasung: ${verglasung}</li>
+      <li>Glastyp: ${glastyp}</li>
+      <li>Maße BxH: ${breite} mm x ${hoehe} mm</li>
+      <li>Höhe Ober/-Unterlicht: ${hoeheOberlicht} mm</li>
+      <li>Fensterbankanschluss: ${fensterbankanschluss}</li>
+      <li>Gesamtpreis: ${amount} €</li>
+      </ul>
+
+
+      <p>Vielen Dank für Ihre Bestellung!<br>
+      Ihr Fenster RTH Team</p>
+    </div>
     `
   };
 
