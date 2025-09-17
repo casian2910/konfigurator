9fce632d59a10e921556a769586469fff9d0091a
casian2910 <casianrazvan2910@gmail.com>
2025-09-17 17:26:56 +0200

    Update email.js

--- api/email.js
+++ api/email.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-export async function sendMail(to, fenstertyp, fluegel, unterlicht, amount, order_number) {
+export async function sendMail(to, nume, strasse, plzOrt, fenstertyp, fluegel, unterlicht, amount, order_number) {
   const transporter = nodemailer.createTransport({
     host: "smtp.ionos.de",
     port: 465, // poți încerca și 587 dacă nu merge cu 465
