6f7a9062933fee92a886d32ce28407e515b0906e
casian2910 <casianrazvan2910@gmail.com>
2025-09-19 13:27:14 +0200

    Update email.js

--- api/email.js
+++ api/email.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-export async function sendMail(to, nume, strasse, plzOrt, hersteller, fensterart, aussenfarbe, innenfarbe, verglasung, glastyp, breite, hoehe, hoeheOberlicht, fensterbankanschluss, fenstertyp, amount, order_number) {
+export async function sendMail(to, nume, strasse, plzOrt, fenstertyp, amount, order_number, summaryHTML) {
   const transporter = nodemailer.createTransport({
     host: "smtp.ionos.de",
     port: 465, // poți încerca și 587 dacă nu merge cu 465
@@ -11,7 +11,7 @@ export async function sendMail(to, nume, strasse, plzOrt, hersteller, fensterart
     }
   });
  
-  const mailOptions = {
+   const mailOptions = {
     from: `"Fenster-RTH" <${process.env.EMAIL_USER}>`,
     to: to,
     subject: "Ihre Bestellbestätigung",
@@ -19,11 +19,13 @@ export async function sendMail(to, nume, strasse, plzOrt, hersteller, fensterart
      <div style="font-family: Arial, sans-serif; line-height: 1.5; color: #333;">
       <!-- Logo -->
       <div style="text-align:center; margin-bottom:20px;">
-        <img src="https://cdn.shopify.com/s/files/1/0946/0301/8572/files/logo_transparent.png?v=1747438787" alt="Fenster RTH Logo" style="max-width:200px;">
+        <img src="https://cdn.shopify.com/s/files/1/0946/0301/8572/files/logo_transparent.png?v=1747438787" 
+             alt="Fenster RTH Logo" style="max-width:200px;">
       </div>
 
       <p>Sehr geehrter Kunde,</p>
-      <p>vielen Dank für Ihr Vertrauen, das Sie uns mit Ihrer Bestellung entgegengebracht haben. Wir bestätigen hiermit nachfolgenden Auftrag. Es gelten unsere AGB.</p>
+      <p>vielen Dank für Ihr Vertrauen, das Sie uns mit Ihrer Bestellung entgegengebracht haben. 
+         Wir bestätigen hiermit nachfolgenden Auftrag. Es gelten unsere AGB.</p>
 
       <p><strong>Bestellnummer:</strong> ${order_number}</p>
 
@@ -32,24 +34,12 @@ export async function sendMail(to, nume, strasse, plzOrt, hersteller, fensterart
         ${nume}<br>
         ${strasse}<br>
         ${plzOrt}<br>
-        </p>
-
+      </p>
 
       <h3>Ihre bestellten Artikel</h3>
-      <ul>
-      <li>Hersteller: ${hersteller}</li>
-      <li>Fensterart: ${fensterart}</li>
-      <li>Fensteröffnung: ${fenstertyp}</li>
-      <li>Außenfarbe: ${aussenfarbe}</li>
-      <li>Innenfarbe: ${innenfarbe}</li>
-      <li>Verglasung: ${verglasung}</li>
-      <li>Glastyp: ${glastyp}</li>
-      <li>Maße BxH: ${breite} mm x ${hoehe} mm</li>
-      <li>Höhe Ober/-Unterlicht: ${hoeheOberlicht} mm</li>
-      <li>Fensterbankanschluss: ${fensterbankanschluss}</li>
-      <li>Gesamtpreis: ${amount} €</li>
-      </ul>
+      ${summaryHTML}  
 
+      <p><b>Gesamtpreis:</b> ${amount} €</p>
 
       <p>Vielen Dank für Ihre Bestellung!<br>
       Ihr Fenster RTH Team</p>
