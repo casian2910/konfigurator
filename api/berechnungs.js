d046b7dfc29c2b01178576b8bb6c92fb1d187478
casian2910 <casianrazvan2910@gmail.com>
2025-10-03 10:47:31 +0200

    Update berechnungs.js

--- api/berechnungs.js
+++ api/berechnungs.js
@@ -81,15 +81,15 @@ if (fenstertyp4 === "Festverglasung") {
 
   // 2 Flügel mit Unterlicht
 if (fenstertyp5 === "Festverglasung") {
-  pret += suprafataTotala * 30;  // exemplu +5 EUR/m²
+  pret += suprafataTotala * 10;  // exemplu +5 EUR/m²
 } else if (fenstertyp5 === "drehkipp Links und Fest Unterlicht" || fenstertyp5 === "drehkipp Rechts und Fest Unterlicht") {
-  pret += suprafataTotala * 85; // exemplu +30 EUR/m²
+  pret += suprafataTotala * 20; // exemplu +30 EUR/m²
 } else if (fenstertyp5 === "Drehkipp Links und drehkipp Rechst mit Pfosten") {
-  pret += suprafataTotala * 105; // exemplu +30 EUR/m²
+  pret += suprafataTotala * 30; // exemplu +30 EUR/m²
 } else if (fenstertyp5 === "Drehkipp Links und drehkipp Rechst mit Stulp") {
-  pret += suprafataTotala * 98; // exemplu +30 EUR/m²
+  pret += suprafataTotala * 30; // exemplu +30 EUR/m²
 } else if (fenstertyp5 === "Drehkipp Rechts und dreh Links mit Stulp") {
-  pret += suprafataTotala * 98; // exemplu +30 EUR/m²
+  pret += suprafataTotala * 30; // exemplu +30 EUR/m²
 }
   
   // 5. Verglasung (3-Fach = extra pe m²)
@@ -121,6 +121,7 @@ if (!eAlbAussen && !eAlbInnen) {
 
   return res.status(200).json({ pret });
 }
+
 
 
 
