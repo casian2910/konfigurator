046f4a1f9cc420028446f5c96e4c343b2e74b717
casian2910 <casianrazvan2910@gmail.com>
2025-10-03 11:46:07 +0200

    Update berechnungs.js

--- api/berechnungs.js
+++ api/berechnungs.js
@@ -49,7 +49,7 @@ pret += suprafataTotala * (tarifBazaPeMp[hersteller] || 0);
 if (fenstertyp === "Festverglasung") {
   pret += suprafataTotala * 24;
 } else if (fenstertyp === "Links" || fenstertyp === "Rechts") {
-  pret += 110 + suprafataTotala * 40;
+  pret += 120 + suprafataTotala * 30;
 }
 
   // Oberlicht
@@ -121,6 +121,7 @@ if (!eAlbAussen && !eAlbInnen) {
 
   return res.status(200).json({ pret });
 }
+
 
 
 
