bc2caf017aaebcf29d22ca8d2bf38116da82a614
casian2910 <casianrazvan2910@gmail.com>
2025-10-17 15:33:21 +0200

    Create produkte.js

---
+++ api/produkte.js
@@ -0,0 +1,31 @@
+// produkte.js
+const produse = [
+  {
+    id: "f001",
+    titlu: "Fenster Standard",
+    pret_unitar: 150,
+    poza_fereastra: "/images/fenster1.png",
+    profil: "PVC",
+    tip_fereastra: "Einflügelig",
+    fenstertyp: "Öffnung nach innen",
+    culoare_exterior: "Weiß",
+    culoare_interior: "Weiß",
+    tip_geam: "Doppelt",
+    glastyp: "klar",
+    dimensiuni: "120x150 cm",
+    dimensiuni2: "Oberlicht 50x30 cm",
+    fensterbankanschluss: "Standard"
+  },
+  {
+    id: "z001",
+    titlu: "Rolladen",
+    pret_unitar: 50,
+    poza_fereastra: "/images/rolladen.png"
+  },
+  {
+    id: "z002",
+    titlu: "Griffset",
+    pret_unitar: 20,
+    poza_fereastra: "/images/griff.png"
+  }
+];
