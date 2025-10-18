f86c99e724b9e5875be643c26d7ba87f7011ca38
casian2910 <casianrazvan2910@gmail.com>
2025-10-18 13:19:47 +0200

    Update produkt-liste.js

--- public/produkt-liste.js
+++ public/produkt-liste.js
@@ -56,12 +56,11 @@ function adaugaInCos(handle) {
       cos.push(produsNou);
       localStorage.setItem("cos_produse", JSON.stringify(cos));
 
-      // adaugă și în Shopify
-      fetch(`/cart/add.js`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ items: [{ quantity: 1, id: prod.variants[0].id }] })
-      });
+      fetch('/cart/add.js', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ items: [{ quantity: 1, id: prod.variants[0].id }] })
+})
 
       // redirect fix către pagina ta de checkout
       window.location.href = '/pages/bezahlung';
