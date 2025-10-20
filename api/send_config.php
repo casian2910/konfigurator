8e9ed9c529f3fdb3385e2bc69a3cf6fc09a2758c
casian2910 <casianrazvan2910@gmail.com>
2025-10-20 22:28:29 +0200

    Update send_config.php

--- api/send_config.php
+++ api/send_config.php
@@ -1,1 +1,23 @@
+<?php
+// Citim datele trimise din JavaScript
+$data = json_decode(file_get_contents("php://input"), true);
+$rezumat = $data['rezumat'];
 
+// Setăm emailul unde vrei să primești notificările
+$to = "info@fenster-rth.de";  // ← schimbă cu emailul tău real
+$subject = "Nouă configurare de fereastră";
+
+// Construim mesajul
+$message = "Un client a adăugat în coș următoarea configurație:\n\n";
+$message .= strip_tags($rezumat); // elimină tag-urile HTML pentru claritate
+
+$headers = "From: no-reply@fenster-rth.de\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+// Trimitem emailul
+if (mail($to, $subject, $message, $headers)) {
+  echo "Trimis cu succes";
+} else {
+  echo "Eroare la trimitere";
+}
+?>
